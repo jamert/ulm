35a1f664afa86510828d379f7b80ffe9609a8c52
Artem Daineko <dayneko.ab@gmail.com>
2023-10-18 00:11:33 +0600

    Fix types in ulm.checkpoint

--- src/ulm/checkpoint.py
+++ src/ulm/checkpoint.py
@@ -1,19 +1,21 @@
 import contextlib
 import pickle
 from pathlib import Path
-from typing import Iterable, Optional
+from typing import Any, Generator, Iterable, Optional
 
 
 class Singular:
     def __init__(self, filename: Path) -> None:
         self.filename = filename
 
-    def saved(self) -> any:
+    def saved(self) -> Any:
         if self.filename.exists():
             with self.filename.open("rb") as fd:
                 return pickle.load(fd)
+        else:
+            return None
 
-    def save(self, value: any) -> any:  # TODO: generic type
+    def save(self, value: Any) -> Any:  # TODO: generic type
         with self.filename.open("wb") as fd:
             pickle.dump(value, fd)
 
@@ -21,7 +23,7 @@ class Singular:
 
 
 @contextlib.contextmanager
-def singular(filename: Path):
+def singular(filename: Path) -> Generator[Singular, None, None]:
     yield Singular(filename)
 
 
@@ -31,7 +33,7 @@ class indexed:
         filename: Path,
         iterable: Iterable,
         once_in: Optional[int] = None,
-        initializer: Optional[any] = None,
+        initializer: Optional[Any] = None,
     ) -> None:
         self.filename = filename
         self.iterable = iterable.__iter__()
@@ -52,11 +54,11 @@ class indexed:
     def __iter__(self) -> "indexed":
         return self
 
-    def __next__(self) -> tuple[int, any, any]:
+    def __next__(self) -> tuple[int, Any, Any]:
         self._index += 1
         return self._index, next(self.iterable), self._context
 
-    def save(self, index: int, context: any) -> any:  # TODO: generic type
+    def save(self, index: int, context: Any) -> Any:  # TODO: generic type
         if self.once_in is None or (index + 1) % self.once_in == 0:
             with self.filename.open("wb") as fd:
                 pickle.dump((index, context), fd)
@@ -65,7 +67,7 @@ class indexed:
 
         return context
 
-    def saved(self) -> any:
+    def saved(self) -> Any:
         """
         Marks finish of iteration
         """
